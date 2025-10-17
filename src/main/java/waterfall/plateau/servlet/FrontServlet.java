ea855588d75f55f0ca41dfe26526e93b02dd328d
Fanekena <fanekenaraharijaona@gmail.com>
2025-10-17 16:20:06 +0300

    Code refactoring for FrontServlet

--- src/main/java/waterfall/plateau/servlet/FrontServlet.java
+++ src/main/java/waterfall/plateau/servlet/FrontServlet.java
@@ -2,16 +2,13 @@ package waterfall.plateau.servlet;
 
 import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
-import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.Optional;
 
-@WebServlet("/")
 public class FrontServlet extends HttpServlet {
     private RequestDispatcher contextDefaultDispatcher;
 
@@ -25,22 +22,14 @@ public class FrontServlet extends HttpServlet {
     @Override
     public void service(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        String relativePath = getContextRelativePath(request);
-
-        if (getServletContext().getResource(relativePath) != null) contextDefaultDispatcher.forward(request, response);
+        String servletPath = request.getServletPath();
+        if (getServletContext().getResource(servletPath) != null)
+            contextDefaultDispatcher.forward(request, response);
         else {
             response.setContentType("text/plain;charset=UTF-8");
             try (PrintWriter printWriter = response.getWriter()) {
-                printWriter.print(relativePath);
+                printWriter.print(servletPath);
             }
         }
-    }
-
-    // TODO security concerns around path traversal with request containing ".." or/and "//"
-    private String getContextRelativePath(HttpServletRequest request) {
-        String servletPath = request.getServletPath(); // "" for "/"
-        String pathInfo = Optional.ofNullable(request.getPathInfo()).orElse(""); // null for "/"
-        String relativePath = servletPath + pathInfo;
-        return relativePath.isEmpty() ? "/" : relativePath;
     }
 }
