a081928811b642e0afd7bfdf012856239e151431
Fanekena <fanekenaraharijaona@gmail.com>
2025-11-12 17:05:14 +0300

    sprint-5 done

--- src/main/java/waterfall/view/ModelView.java
+++ src/main/java/waterfall/view/ModelView.java
@@ -1,7 +1,15 @@
 package waterfall.view;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class ModelView {
     private String view;
+    private Map<String, Object> attributes;
+
+    public ModelView() {
+        attributes = new HashMap<>();
+    }
 
     public String getView() {
         return view;
@@ -9,5 +17,13 @@ public class ModelView {
 
     public void setView(String view) {
         this.view = view;
+    }
+
+    public void setAttribute(String attribute, Object value) {
+        attributes.put(attribute, value);
+    }
+
+    public Map<String, Object> getAttributes() {
+        return attributes;
     }
 }
