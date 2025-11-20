2b1e1cb98c05447e6b2aac1ed2f39f222ed1871c
Fanekena <fanekenaraharijaona@gmail.com>
2025-11-20 16:32:47 +0300

    Update ArgumentResolver.java

--- src/main/java/waterfall/core/reflection/ArgumentResolver.java
+++ src/main/java/waterfall/core/reflection/ArgumentResolver.java
@@ -16,17 +16,16 @@ public final class ArgumentResolver {
         for (int i = 0; i < params.length; i++) {
             Parameter param = params[i];
 
-
             if (param.isAnnotationPresent(RequestParam.class)) {
                 RequestParam reqParam = param.getAnnotation(RequestParam.class);
                 String paramValue = req.getParameter(reqParam.name());
-                // TODO The framework only supports String here
+                // TODO The framework only supports String for now
                 Object value = paramValue;
                 args[i] = value;
                 continue;
             } else if (req.getParameter(param.getName()) != null) {
                 String paramValue = req.getParameter(param.getName());
-                // TODO The framework only supports String here
+                // TODO The framework only supports String for now
                 Object value = paramValue;
                 args[i] = value;
                 continue;
