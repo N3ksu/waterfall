dd528fa0ecf9c375db39a1d1d28643ac7f3c1404
Fanekena <fanekenaraharijaona@gmail.com>
2025-11-21 15:09:48 +0300

    code refactor

--- src/main/java/waterfall/util/parser/StringParser.java
+++ src/main/java/waterfall/util/parser/StringParser.java
@@ -7,31 +7,38 @@ import java.time.YearMonth;
 
 public final class StringParser {
     public static Object parse(String s, Class<?> c) {
+        // String
+        if (c.equals(String.class))
+            return s;
+
+        // Numeric value
         if (c.equals(int.class) || c.equals(Integer.class))
             return Integer.parseInt(s);
 
         if (c.equals(long.class) || c.equals(Long.class))
             return Long.parseLong(s);
 
-        if (c.equals(float.class) || c.equals(Float.class))
-            return Float.parseFloat(s);
-
         if (c.equals(double.class) || c.equals(Double.class))
             return Double.parseDouble(s);
 
+        if (c.equals(float.class) || c.equals(Float.class))
+            return Float.parseFloat(s);
+
         // TODO Maybe we should provide the client a wrapper for those complex data type
-        if (c.equals(LocalTime.class))
-            return LocalTime.parse(s); // time
 
-        if (c.equals(YearMonth.class))
-            return YearMonth.parse(s); // month
-
+        // Time related value
         if (c.equals(LocalDate.class))
             return LocalDate.parse(s); // date
+
+        if (c.equals(LocalTime.class))
+            return LocalTime.parse(s); // time
 
         if (c.equals(LocalDateTime.class))
             return LocalDateTime.parse(s); // datetime-local
 
-        return s; // If we cannot parse it into another class than String, we return the String itself
+        if (c.equals(YearMonth.class))
+            return YearMonth.parse(s); // month
+
+        return null; // Non-parsable String will be nullified
     }
 }
